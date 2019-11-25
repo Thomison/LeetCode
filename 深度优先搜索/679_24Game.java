5e0a2123c64c9e59c034c4d936104c90bcff7e3c
tangyiheng <1021953309tyh@gmail.com>
2019-11-25 23:12:37 +0800

    create 679_24Game.java

---
+++ 深度优先搜索/679_24Game.java
@@ -0,0 +1,17 @@
+//dfs构造出可能得算术表达式情况，然后用逆波兰表达式求解，判断每种情况是否等于24
+class Solution {
+    char[] operators={'+','-','*','/','(',')'};
+    
+    public boolean judgePoint24(int[] nums) {
+        
+    }
+    /*
+    input:  str
+    output: int
+    */
+    int compute(String str) {
+        //操作数范围为0~9
+        char[] chars=str.toCharArray();
+        
+    }
+}
