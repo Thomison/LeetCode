e523cbac85f757bbb9f6d28d66e061e9f90ed8f4
Thomison <1021953309tyh@gmail.com>
2020-07-01 01:44:48 +0800

    Default Changelist

---
+++ leetcode/editor/cn/[]合并两个有序数组.java
@@ -0,0 +1,51 @@
+//给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。 
+//
+// 
+//
+// 说明: 
+//
+// 
+// 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。 
+// 你可以假设 nums1 有足够的空间（空间大小大于或等于 m + n）来保存 nums2 中的元素。 
+// 
+//
+// 
+//
+// 示例: 
+//
+// 输入:
+//nums1 = [1,2,3,0,0,0], m = 3
+//nums2 = [2,5,6],       n = 3
+//
+//输出: [1,2,2,3,5,6] 
+// Related Topics 数组 双指针
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    // 要求：不适用额外空间，时间复杂度为O(m+n)
+    // 从后往前合并，巧妙避免了移动元素造成的低效率
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        if (i < 0) {
+            while (j >= 0) {
+                nums1[k--] = nums2[j--];
+            }
+        }
+        /*if (j < 0) {
+            for (; i >= 0; i--) {
+                nums1[k--] = nums1[i--];
+            }
+        }*/
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
